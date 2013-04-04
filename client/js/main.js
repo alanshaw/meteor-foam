25471cb0f4ad86b3e6d4da235d84249661cbdcaa
olizilla <oli@zilla.org.uk>
2013-04-04 18:34:58 +0100

    Click to pop bubbles and kill photos.

--- client/js/main.js
+++ client/js/main.js
@@ -3,7 +3,7 @@ Meteor.startup(function() {
 	var booth = $('#booth'),
 		canvas = $('canvas', booth)[0],
 		context = canvas.getContext('2d'),
-		video = $('video', booth)[0],
+		video = $('video', booth)[0]
 	
 	navigator.getUserMedia = navigator.getUserMedia || navigator.mozGetUserMedia || navigator.webkitGetUserMedia
 	window.URL = window.URL || window.mozURL || window.webkitURL;
@@ -102,6 +102,7 @@ function renderPhotos() {
 		.append('g')
 		.attr('class', 'photo')
 		.attr('transform', 'translate(' + (width / 2) + ', ' + (height / 2) + ')')
+		.on("click", function(d) { Photos.remove(d._id); });
 	
 	photoEnter.append('clipPath')
 		.attr('id', function(d) { return 'cp-' + d._id; })
@@ -124,12 +125,8 @@ function renderPhotos() {
 		.attr('cx', function(d){ return d.r; })
 		.attr('cy', function(d){ return d.r; });
 	
-	var photoExit = photo.exit()
-		.attr('transform', 'translate(' + (width / 2) + ', ' + (height / 2) + ')');
-	
-	photoExit.select('image')
-		.attr('width', 0)
-		.attr('height', 0);
+	var photoExit = photo.exit();
+	photoExit.transition().remove().select('circle').attr('r', 0);
 }
 
 function getValue(time) {
